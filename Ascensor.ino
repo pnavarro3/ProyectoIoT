9c899373408473bd7f2911c11fb01030d2378628
pnavarro3 <alu0101844633@ull.edu.es>
2025-11-19 07:52:30 +0000

    Modify WiFi credentials and enhance web server functionality
    Updated WiFi credentials and improved HTML response handling with AJAX for motor control.

--- Ascensor.ino
+++ Ascensor.ino
@@ -1,24 +1,20 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-// ⚡ Configuración WiFi
-const char* ssid = "TU_SSID";
-const char* password = "TU_PASSWORD";
+const char* ssid = "yiyiyi";
+const char* password = "xabicrack";
 
-// ⚡ Pines de relés
 const int RELE_ESTRELLA = 5;
 const int RELE_TRIANGULO = 18;
 const int RELE_DERECHA = 19;
 const int RELE_IZQUIERDA = 21;
 
-// ⚡ Variables de estado
 String estadoMotor = "Parado";
 String estadoArranque = "-";
 String sentidoGiro = "-";
 
 WebServer server(80);
 
-// 🛑 Apagar todo
 void apagarTodo() {
   digitalWrite(RELE_ESTRELLA, LOW);
   digitalWrite(RELE_TRIANGULO, LOW);
@@ -29,11 +25,8 @@ void apagarTodo() {
   sentidoGiro = "-";
 }
 
-// 🚀 Arrancar motor
 void arrancarMotor(bool derecha) {
-  apagarTodo(); // seguridad
-
-  // Sentido de giro
+  apagarTodo();
   if (derecha) {
     digitalWrite(RELE_DERECHA, HIGH);
     sentidoGiro = "Derecha";
@@ -41,27 +34,29 @@ void arrancarMotor(bool derecha) {
     digitalWrite(RELE_IZQUIERDA, HIGH);
     sentidoGiro = "Izquierda";
   }
-
-  // Arranque en estrella
   digitalWrite(RELE_ESTRELLA, HIGH);
   estadoArranque = "Estrella";
   estadoMotor = "Arrancado";
-
-  delay(3000); // esperar 3 segundos
-
-  // Conmutar a triángulo
+  delay(3000);
   digitalWrite(RELE_ESTRELLA, LOW);
   digitalWrite(RELE_TRIANGULO, HIGH);
   estadoArranque = "Triángulo";
 }
 
-// 🌐 Página web
+// Página principal con AJAX
 String paginaHTML() {
-  String html = "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>Control Motor</title></head><body>";
+  String html = "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>Control Motor</title>";
+  html += "<script>";
+  html += "function actualizar(){fetch('/estado').then(r=>r.json()).then(d=>{";
+  html += "document.getElementById('estado').innerText=d.estado;";
+  html += "document.getElementById('sentido').innerText=d.sentido;";
+  html += "document.getElementById('arranque').innerText=d.arranque;});}";
+  html += "setInterval(actualizar,1000);"; // cada 1 segundo
+  html += "</script></head><body>";
   html += "<h1>Control Motor Estrella-Triángulo</h1>";
-  html += "<p><b>Estado:</b> " + estadoMotor + "</p>";
-  html += "<p><b>Sentido:</b> " + sentidoGiro + "</p>";
-  html += "<p><b>Arranque:</b> " + estadoArranque + "</p>";
+  html += "<p><b>Estado:</b> <span id='estado'>" + estadoMotor + "</span></p>";
+  html += "<p><b>Sentido:</b> <span id='sentido'>" + sentidoGiro + "</span></p>";
+  html += "<p><b>Arranque:</b> <span id='arranque'>" + estadoArranque + "</span></p>";
   html += "<form action='/derecha'><button>Arrancar Derecha</button></form>";
   html += "<form action='/izquierda'><button>Arrancar Izquierda</button></form>";
   html += "<form action='/parar'><button>Parar</button></form>";
@@ -69,56 +64,38 @@ String paginaHTML() {
   return html;
 }
 
-// 📡 Rutas servidor
-void handleRoot() {
-  server.send(200, "text/html", paginaHTML());
-}
+// Rutas
+void handleRoot() { server.send(200, "text/html", paginaHTML()); }
+void handleDerecha() { arrancarMotor(true); server.send(200, "text/html", paginaHTML()); }
+void handleIzquierda() { arrancarMotor(false); server.send(200, "text/html", paginaHTML()); }
+void handleParar() { apagarTodo(); server.send(200, "text/html", paginaHTML()); }
 
-void handleDerecha() {
-  arrancarMotor(true);
-  server.send(200, "text/html", paginaHTML());
-}
-
-void handleIzquierda() {
-  arrancarMotor(false);
-  server.send(200, "text/html", paginaHTML());
-}
-
-void handleParar() {
-  apagarTodo();
-  server.send(200, "text/html", paginaHTML());
+// Endpoint JSON para AJAX
+void handleEstado() {
+  String json = "{\"estado\":\"" + estadoMotor + "\",\"sentido\":\"" + sentidoGiro + "\",\"arranque\":\"" + estadoArranque + "\"}";
+  server.send(200, "application/json", json);
 }
 
 void setup() {
   Serial.begin(115200);
-
-  // Configurar pines
   pinMode(RELE_ESTRELLA, OUTPUT);
   pinMode(RELE_TRIANGULO, OUTPUT);
   pinMode(RELE_DERECHA, OUTPUT);
   pinMode(RELE_IZQUIERDA, OUTPUT);
-
   apagarTodo();
 
-  // Conectar WiFi
   WiFi.begin(ssid, password);
-  Serial.print("Conectando a WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Conectado!");
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  Serial.println("Conectado a WiFi!");
   Serial.println(WiFi.localIP());
 
-  // Configurar servidor
   server.on("/", handleRoot);
   server.on("/derecha", handleDerecha);
   server.on("/izquierda", handleIzquierda);
   server.on("/parar", handleParar);
+  server.on("/estado", handleEstado);
 
   server.begin();
 }
 
-void loop() {
-  server.handleClient();
-}
+void loop() { server.handleClient(); }
